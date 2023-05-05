f33512484499ba4b8b51b9f9faeb6a5f74d7aec5
Pankaj Yadav <pankaj.yadav@zinier.com>
2023-05-05 18:37:17 +0530

    added new images and style to the images

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
-import React from 'react'
-const About=()=> {
+import React from "react";
+const About = () => {
   // const a = useContext(NoteContext)
   // useEffect(() =>{
   //   a.update();
   // },[])
   return (
     <div>
-      this about us who is in class
+      <div classNameName="flex-card-container">
+        <div classNameName="flex-card whole-card-clickable">
+          <a href="/features/notes-app" target="_blank">
+            <div classNameName="header-image">
+              <img src="https://cdn.dribbble.com/assets/art-banners/itsa-me-time_dennis-salvatier-52a50b9a8e40c31aed521869f4cf0cb02ccbc069c0aac9b75efd714a7354d415.jpg" alt="Notes app"/>
+            </div>
+          </a>
+        </div>
+      </div>
+      <h1>Make My Notes</h1>
+      <p>
+        Make My Notes is the best place to jot down quick thoughts or to save
+        longer notes filled with checklists, images, web links, scanned
+        documents, handwritten notes, or sketches. And with iCloud, it's easy to
+        keep all your devices in sync, so you’ll always have your notes with you
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default About;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,15 +14,12 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar navbar-dark bg-primary">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><b>Navbar</b></Link>
+                <Link className="navbar-brand" to="/"><b>Make My Notes</b></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname === "/"? "active":""}`} aria-current="page" to="/"><b>Home</b></Link>
-                        </li>
                         <li className="nav-item">
                             <Link className={`nav-link ${location.pathname === "/about"? "active":""}`} to="/about"><b>About</b></Link>
                         </li>
